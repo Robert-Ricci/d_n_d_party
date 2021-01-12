4b2a828ceafcdd891e6dda5b835ace87db4cee75
Robert Ricci <rmricci121@gmail.com>
2021-01-12 15:20:09 -0500

    This commit created the form for signup

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2021_01_11_202338) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
