bc380f4b6de4262fec656b9147730297a90369c8
Robert Ricci <rmricci121@gmail.com>
2021-01-13 15:01:29 -0500

    This commit added show, index, and new views to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
 
+    def index
+    end
+    
     def new
         @user = User.new
     end
 
     def create
-        @user = User.create 
-            # if @user.save
-        redirect_to @user
+        @user = User.create
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+          else
+            render 'new'
+          end
     end
 
     def show 
