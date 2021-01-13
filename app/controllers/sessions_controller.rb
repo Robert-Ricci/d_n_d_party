9f29ec811d73653e60d0750b83cb010e030614e2
Robert Ricci <rmricci121@gmail.com>
2021-01-13 14:50:25 -0500

    This commit adds new and create to sessions controller -adds show controller, and index and show view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+    
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     def create
         @user = User.create 
             # if @user.save
+        redirect_to @user
+    end
 
+    def show 
+        @user = User.find_by(id: params[:id])
     end
 
 end
