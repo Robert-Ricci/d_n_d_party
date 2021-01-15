97552c7cfa9a21c1bd91d262315cb10b971ad8d9
Robert Ricci <rmricci121@gmail.com>
2021-01-15 14:02:40 -0500

    This commit added a line for race on character new page

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2021_01_14_190633) do
   enable_extension "plpgsql"
 
   create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "race"
+    t.string "klass"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
