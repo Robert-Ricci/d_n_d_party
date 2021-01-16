5e10ecf0265767d9e6ab4eeac8caceb9db2f91f1
Robert Ricci <rmricci121@gmail.com>
2021-01-16 11:04:46 -0500

    This commit adds character creation, but is currently not working

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,11 +1,28 @@
 class CharactersController < ApplicationController
+  
   def new
     @character = Character.new
   end
 
   def create
+    @character = Character.create
+    binding.pry
+    if @character.save
+     
+      redirect_to user_characters_path(@character)
+    else 
+      render 'new'
+    end
   end
 
   def show
+    @characters = Character.all
   end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :race, :klass)
+  end
+
 end
